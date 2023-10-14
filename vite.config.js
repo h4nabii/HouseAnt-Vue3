d104bd88175fc5e99a9565a4d6da5ae5cffd2bc0
h4nabii <leon_xenos.xu@foxmail.com>
2023-10-14 15:41:48 +0800

    remove

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,14 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, "index.html"),
                 database: resolve(__dirname, "database/index.html")
-            }
+            }, output: {
+                // 入口文件名
+                entryFileNames: `assets/[name].js`,
+                // 块文件名
+                chunkFileNames: `assets/[name]-[hash].js`,
+                // 资源文件名 css 图片等等
+                assetFileNames: `assets/[name]-[hash].[ext]`,
+            },
         }
     }
 });
