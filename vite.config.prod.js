b0c2a664355dc2f2ed1cff6ee56d4283cdad52e4
h4nabii <leon_xenos.xu@foxmail.com>
2023-10-19 00:07:26 +0800

    项目重命名，路径修改

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -1,11 +1,10 @@
 import {fileURLToPath, URL} from "node:url";
-import {resolve} from "path";
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/houseant-front",
+    base: "/HouseAnt-Vue3",
     plugins: [
         vue(),
     ],
